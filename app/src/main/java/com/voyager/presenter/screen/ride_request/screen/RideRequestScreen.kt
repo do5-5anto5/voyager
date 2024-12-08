4b2511e5d0809464295c6786c14e96e18d9a1d82
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-12-08 18:57:42 -0300

    feat: Prepare the screen to do the first app Ride request

--- app/src/main/java/com/voyager/presenter/screen/ride_request/screen/RideRequestScreen.kt
+++ app/src/main/java/com/voyager/presenter/screen/ride_request/screen/RideRequestScreen.kt
@@ -1,21 +1,38 @@
 package com.voyager.presenter.screen.ride_request.screen
 
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.Composable
-import org.koin.androidx.compose.koinViewModel
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import com.voyager.data.remote.model.request_body.RideEstimateRequestBody
+import com.voyager.presenter.components.DefaultButton
+import com.voyager.presenter.screen.ride_request.action.RideRequestAction
 import com.voyager.presenter.screen.ride_request.state.RideRequestState
 import com.voyager.presenter.screen.ride_request.viewModel.RideRequestViewModel
-import com.voyager.presenter.screen.ride_request.action.RideRequestAction
+import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun RequestRideScreen() {
 
     val viewModel = koinViewModel<RideRequestViewModel>()
     val state by viewModel.state.collectAsState()
+    val action = viewModel::submitAction
 
+    RequestRideScreenContent(
+        state = state,
+        action = action
+    )
 }
 
 @Composable
@@ -23,6 +40,31 @@ private fun RequestRideScreenContent(
     state: RideRequestState,
     action: (RideRequestAction) -> Unit
 ) {
+
+    Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black)
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            DefaultButton(
+                modifier = Modifier
+                    .padding(40.dp),
+                onClick = { action(RideRequestAction.GetRideEstimate(
+                    RideEstimateRequestBody(
+                        customerId = "CT01",
+                        origin = "Av. Pres. Kenedy, 2385 - Remédios, Osasco - SP,02675-031",
+                        destination = "Av. Paulista, 1538 - Bela Vista, São Paulo - SP, 01310-200"
+                    )
+                ))
+                          },
+                text = "Request Ride"
+            )
+        }
+    }
 
 }
 
