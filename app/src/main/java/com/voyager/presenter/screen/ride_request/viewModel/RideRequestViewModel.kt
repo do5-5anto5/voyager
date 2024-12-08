e7fac2d7845979075b13cfcd36a1f6359cb5b92f
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-12-08 12:23:26 -0300

    feat: initial implementation of getRideEstimateUseCase in RideRequestViewModel - this view model will manage the logic between RideRequestScreen and GetRideEstimateUseCase

--- app/src/main/java/com/voyager/presenter/screen/ride_request/viewModel/RideRequestViewModel.kt
+++ app/src/main/java/com/voyager/presenter/screen/ride_request/viewModel/RideRequestViewModel.kt
@@ -2,17 +2,39 @@ package com.voyager.presenter.screen.ride_request.viewModel
 
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.voyager.domain.usecase.ride.GetRideEstimateUseCase
 import com.voyager.presenter.screen.ride_request.action.RideRequestAction
 import com.voyager.presenter.screen.ride_request.state.RideRequestState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
 
-class RideRequestViewModel : ViewModel() {
+class RideRequestViewModel(
+    private val getRideEstimateUseCase: GetRideEstimateUseCase
+) : ViewModel() {
 
     private val _state = MutableStateFlow(RideRequestState())
     val state = _state.asStateFlow()
 
     fun submitAction(action: RideRequestAction) {
 
+    }
+
+    fun getRideEstimate(
+        customerId: String?,
+        origin: String?,
+        destination: String?
+    ) {
+        try {
+            viewModelScope.launch {
+                getRideEstimateUseCase.invoke(customerId, origin, destination)
+            }
+
+        } catch (exception: HttpException) {
+            exception.printStackTrace()
+            exception.message
+        }
     }
 }
