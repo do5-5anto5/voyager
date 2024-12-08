51a9b6f1bd7447cd61698d65a998d3c1f29e6594
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-12-08 10:15:53 -0300

    feat: Implement domain layer model representation of Ride Estimate
    classes:
    - RideEstimate
    - Address
    - Option
    - Review
    - RideEstimateError

---
+++ app/src/main/java/com/voyager/domain/ride/estimate/RideEstimateError.kt
@@ -0,0 +1,8 @@
+// YApi QuickType插件生成，具体参考文档:https://plugins.jetbrains.com/plugin/18847-yapi-quicktype/documentation
+
+package com.voyager.domain.ride.estimate
+
+data class RideEstimateError (
+    val errorDescription: String,
+    val errorCode: String
+)
