a472e4b8d64b3038f6f034849ec9d86469a43af1
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-12-08 18:55:15 -0300

    feat: set first action and state to Ride request

--- app/src/main/java/com/voyager/presenter/screen/ride_request/state/RideRequestState.kt
+++ app/src/main/java/com/voyager/presenter/screen/ride_request/state/RideRequestState.kt
@@ -1,6 +1,9 @@
 package com.voyager.presenter.screen.ride_request.state
 
+import com.voyager.data.remote.model.request_body.RideEstimateRequestBody
+
 
 data class RideRequestState(
-    val isLoading: Boolean = true
+    val isLoading: Boolean = true,
+    val requestBody: RideEstimateRequestBody = RideEstimateRequestBody()
 )
