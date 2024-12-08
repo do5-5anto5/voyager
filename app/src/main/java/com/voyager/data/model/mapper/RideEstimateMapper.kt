6736ff1307db0d31e872634beb428caf69c2452b
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-12-08 18:32:08 -0300

    feat: implement RideEstimateMapper, that defines extension functions that map data objects from the data layer (remote models) to the domain layer (domain models)

---
+++ app/src/main/java/com/voyager/data/model/mapper/RideEstimateMapper.kt
@@ -0,0 +1,46 @@
+package com.voyager.data.model.mapper
+
+import com.voyager.data.remote.model.ride_estimate_response.AddressResponse
+import com.voyager.data.remote.model.ride_estimate_response.OptionResponse
+import com.voyager.data.remote.model.ride_estimate_response.ReviewResponse
+import com.voyager.data.remote.model.ride_estimate_response.RideEstimateResponse
+import com.voyager.domain.model.ride.Address
+import com.voyager.domain.model.ride.Option
+import com.voyager.domain.model.ride.Review
+import com.voyager.domain.model.ride.RideEstimate
+
+fun RideEstimateResponse.toDomain(): RideEstimate {
+    return RideEstimate(
+        origin = origin.toDomain(),
+        destination = destination.toDomain(),
+        distance = distance,
+        duration = duration,
+        options = options.map { it.toDomain() },
+        routeResponse = routeResponse
+    )
+}
+
+fun AddressResponse.toDomain(): Address {
+    return Address(
+        latitude = latitude,
+        longitude = longitude
+    )
+}
+
+fun OptionResponse.toDomain(): Option {
+    return Option(
+        review= review.toDomain(),
+        name= name,
+        description= description,
+        id= id,
+        value= value,
+        vehicle= vehicle
+    )
+}
+
+fun ReviewResponse.toDomain(): Review {
+    return Review(
+        rating = rating,
+        comment = comment
+    )
+}
