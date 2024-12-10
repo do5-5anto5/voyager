4b215a0942640a519dfdce0dc572e4c8beb40ea1
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-12-10 06:38:54 -0300

    feat: Implement first approach to RideRequestScreen formulary

--- app/src/main/java/com/voyager/presenter/screen/ride_request/screen/RideRequestScreen.kt
+++ app/src/main/java/com/voyager/presenter/screen/ride_request/screen/RideRequestScreen.kt
@@ -2,21 +2,43 @@ package com.voyager.presenter.screen.ride_request.screen
 
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
-import com.voyager.data.remote.model.request_body.RideEstimateRequestBody
+import androidx.compose.ui.unit.sp
+import com.voyager.R
 import com.voyager.presenter.components.DefaultButton
+import com.voyager.presenter.components.DefaultTextView
 import com.voyager.presenter.screen.ride_request.action.RideRequestAction
 import com.voyager.presenter.screen.ride_request.state.RideRequestState
 import com.voyager.presenter.screen.ride_request.viewModel.RideRequestViewModel
@@ -41,6 +63,29 @@ private fun RequestRideScreenContent(
     action: (RideRequestAction) -> Unit
 ) {
 
+    var originMenuExpanded = remember { mutableStateOf(false) }
+    var destinationMenuExpanded = remember { mutableStateOf(false) }
+    val scrollState = rememberScrollState()
+
+    val originMenuLabel = remember { mutableIntStateOf(R.string.choose_origin_address) }
+    val destinationMenuLabel = remember { mutableIntStateOf(R.string.choose_origin_address) }
+    val addresses = listOf(
+        R.string.pres_kenedy_address,
+        R.string.paulista_address,
+        R.string.thomas_edison_address,
+        R.string.av_brasil_address
+    )
+
+
+    LaunchedEffect(originMenuExpanded.value, destinationMenuExpanded.value) {
+        if (originMenuExpanded.value) {
+            scrollState.scrollTo(scrollState.maxValue)
+        }
+        if (destinationMenuExpanded.value) {
+            scrollState.scrollTo(scrollState.maxValue)
+        }
+    }
+
     Scaffold { innerPadding ->
         Column(
             modifier = Modifier
@@ -50,17 +95,117 @@ private fun RequestRideScreenContent(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+
+            DefaultTextView(
+                value = state.id,
+                onValueChange = { action(RideRequestAction.ChangeName(
+                    value = it
+                )) },
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(30.dp)
+                    .height(58.dp)
+                    .clip(CircleShape)
+                    .background(Color.Gray)
+                    .clickable { originMenuExpanded.value = !originMenuExpanded.value },
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(originMenuLabel.intValue),
+                    style = TextStyle(
+                        fontSize = 18.sp,
+                        lineHeight = 19.sp,
+                        textAlign = TextAlign.Center
+                    )
+                )
+                DropdownMenu(
+                    expanded = originMenuExpanded.value,
+                    onDismissRequest = { originMenuExpanded.value = false },
+                    scrollState = scrollState
+                ) {
+                    addresses.forEach { address ->
+                        val chosenOrigin = stringResource(address)
+                        DropdownMenuItem(
+                            text = {
+                                Text(
+                                    text = stringResource(address),
+                                    style = TextStyle(
+                                        fontSize = 18.sp,
+                                        lineHeight = 19.sp,
+                                        textAlign = TextAlign.Center
+                                    ),
+                                )
+                            },
+                            onClick = {
+                                originMenuLabel.intValue = address
+                                originMenuExpanded.value = !originMenuExpanded.value
+                                action(RideRequestAction.ChangeOrigin(address = chosenOrigin))
+                            },
+                            leadingIcon = { Icon(Icons.Outlined.Edit, contentDescription = null) }
+                        )
+                    }
+                }
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(30.dp)
+                    .height(58.dp)
+                    .clip(CircleShape)
+                    .background(Color.Red)
+                    .clickable { destinationMenuExpanded.value = !destinationMenuExpanded.value },
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(destinationMenuLabel.intValue),
+                    style = TextStyle(
+                        fontSize = 18.sp,
+                        lineHeight = 19.sp,
+                        textAlign = TextAlign.Center
+                    )
+                )
+
+                DropdownMenu(
+                    expanded = destinationMenuExpanded.value,
+                    onDismissRequest = { destinationMenuExpanded.value = false },
+                    scrollState = scrollState
+                ) {
+                    addresses.forEach { address ->
+                        val chosenDestination = stringResource(address)
+                        DropdownMenuItem(
+                            text = {
+                                Text(
+                                    text = stringResource(address),
+                                    style = TextStyle(
+                                        fontSize = 18.sp,
+                                        lineHeight = 19.sp,
+                                        textAlign = TextAlign.Center
+                                    ),
+                                )
+                            },
+                            onClick = {
+                                destinationMenuLabel.intValue = address
+                                destinationMenuExpanded.value = !destinationMenuExpanded.value
+                                action(RideRequestAction.ChangeDestination(address = chosenDestination))
+                            },
+                            leadingIcon = { Icon(Icons.Outlined.Edit, contentDescription = null) }
+                        )
+                    }
+                }
+            }
+
             DefaultButton(
                 modifier = Modifier
-                    .padding(40.dp),
-                onClick = { action(RideRequestAction.GetRideEstimate(
-                    RideEstimateRequestBody(
-                        customerId = "CT01",
-                        origin = "Av. Pres. Kenedy, 2385 - Remédios, Osasco - SP,02675-031",
-                        destination = "Av. Paulista, 1538 - Bela Vista, São Paulo - SP, 01310-200"
-                    )
-                ))
-                          },
+                    .padding(30.dp),
+                onClick = {
+                    action(RideRequestAction.GetRideEstimate)
+                },
                 text = "Request Ride"
             )
         }
