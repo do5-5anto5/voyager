5499aea1e4699a9dd2a48dcaa14bbd679bc43403
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-12-11 13:45:05 -0300

    chore: inject RideConfirmUseCase and RideConfirmViewModel

--- app/src/main/java/com/voyager/dependency_injection/UseCaseModule.kt
+++ app/src/main/java/com/voyager/dependency_injection/UseCaseModule.kt
@@ -1,8 +1,13 @@
 package com.voyager.dependency_injection
 
 import com.voyager.domain.usecase.GetRideEstimateUseCase
+import com.voyager.domain.usecase.RideConfirmUseCase
 import org.koin.dsl.module
 
 val useCaseModule = module {
+
     factory { GetRideEstimateUseCase(repository = get()) }
+
+    factory { RideConfirmUseCase(repository = get()) }
+
 }
