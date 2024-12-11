231f8d2008387ad63bac880134f89df20ce1d18d
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-12-11 09:48:10 -0300

    feat: add action RideRequestAction.ResetErrorState

--- app/src/main/java/com/voyager/presenter/screen/ride_request/action/RideRequestAction.kt
+++ app/src/main/java/com/voyager/presenter/screen/ride_request/action/RideRequestAction.kt
@@ -2,6 +2,7 @@ package com.voyager.presenter.screen.ride_request.action
 
 sealed class RideRequestAction {
     data object GetRideEstimate : RideRequestAction()
+    data object ResetErrorState : RideRequestAction()
     data class ChangeOrigin(val address: String) : RideRequestAction()
     data class ChangeDestination(val address: String) : RideRequestAction()
     data class ChangeName(val value: String) : RideRequestAction()
