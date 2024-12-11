5a047f4114a921dd581328a32796eec8c1c6ffc3
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-12-11 10:58:26 -0300

    task: implement FeedBackUI in RideRequestScreen. - also change the icon from address choose field - also add addresses options

--- app/src/main/java/com/voyager/presenter/screen/ride_request/screen/RideRequestScreen.kt
+++ app/src/main/java/com/voyager/presenter/screen/ride_request/screen/RideRequestScreen.kt
@@ -1,6 +1,7 @@
 package com.voyager.presenter.screen.ride_request.screen
 
 
+import android.content.Context
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -12,12 +13,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -26,10 +28,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
@@ -39,9 +44,13 @@ import androidx.compose.ui.unit.sp
 import com.voyager.R
 import com.voyager.presenter.components.DefaultButton
 import com.voyager.presenter.components.DefaultTextView
+import com.voyager.presenter.components.FeedbackUI
 import com.voyager.presenter.screen.ride_request.action.RideRequestAction
+import com.voyager.presenter.screen.ride_request.action.RideRequestAction.ResetErrorState
 import com.voyager.presenter.screen.ride_request.state.RideRequestState
 import com.voyager.presenter.screen.ride_request.viewModel.RideRequestViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -51,16 +60,26 @@ fun RequestRideScreen() {
     val state by viewModel.state.collectAsState()
     val action = viewModel::submitAction
 
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+
     RequestRideScreenContent(
         state = state,
-        action = action
+        action = action,
+        context = context,
+        scope = scope,
+        snackbarHostState = snackbarHostState
     )
 }
 
 @Composable
 private fun RequestRideScreenContent(
     state: RideRequestState,
-    action: (RideRequestAction) -> Unit
+    action: (RideRequestAction) -> Unit,
+    context: Context,
+    scope: CoroutineScope,
+    snackbarHostState: SnackbarHostState
 ) {
 
     var originMenuExpanded = remember { mutableStateOf(false) }
@@ -68,25 +87,54 @@ private fun RequestRideScreenContent(
     val scrollState = rememberScrollState()
 
     val originMenuLabel = remember { mutableIntStateOf(R.string.choose_origin_address) }
-    val destinationMenuLabel = remember { mutableIntStateOf(R.string.choose_origin_address) }
+    val destinationMenuLabel = remember { mutableIntStateOf(R.string.choose_destination_address) }
     val addresses = listOf(
         R.string.pres_kenedy_address,
         R.string.paulista_address,
         R.string.thomas_edison_address,
-        R.string.av_brasil_address
+        R.string.av_brasil_address,
+        R.string.any_origin_address,
+        R.string.any_destination_address
     )
 
 
-    LaunchedEffect(originMenuExpanded.value, destinationMenuExpanded.value) {
+    LaunchedEffect(originMenuExpanded.value, destinationMenuExpanded.value, state.hasFeedBack) {
         if (originMenuExpanded.value) {
             scrollState.scrollTo(scrollState.maxValue)
         }
         if (destinationMenuExpanded.value) {
             scrollState.scrollTo(scrollState.maxValue)
         }
+
+        scope.launch {
+            if (state.hasFeedBack) {
+                val result = snackbarHostState
+                    .showSnackbar(
+                        message = state.feedBack?.second
+                            ?: context.getString(R.string.generic_error)
+                    )
+                if (result == SnackbarResult.Dismissed) {
+                    action(ResetErrorState)
+                }
+            }
+        }
     }
 
-    Scaffold { innerPadding ->
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(
+                hostState = snackbarHostState,
+                snackbar = { snackbarData ->
+                    state.feedBack?.let { feedbackUI ->
+                        FeedbackUI(
+                            message = snackbarData.visuals.message,
+                            type = feedbackUI.first
+                        )
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -98,9 +146,13 @@ private fun RequestRideScreenContent(
 
             DefaultTextView(
                 value = state.id,
-                onValueChange = { action(RideRequestAction.ChangeName(
-                    value = it
-                )) },
+                onValueChange = {
+                    action(
+                        RideRequestAction.ChangeName(
+                            value = it
+                        )
+                    )
+                },
             )
 
             Row(
@@ -145,7 +197,12 @@ private fun RequestRideScreenContent(
                                 originMenuExpanded.value = !originMenuExpanded.value
                                 action(RideRequestAction.ChangeOrigin(address = chosenOrigin))
                             },
-                            leadingIcon = { Icon(Icons.Outlined.Edit, contentDescription = null) }
+                            leadingIcon = {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.ic_location_line),
+                                    contentDescription = null
+                                )
+                            }
                         )
                     }
                 }
@@ -194,7 +251,12 @@ private fun RequestRideScreenContent(
                                 destinationMenuExpanded.value = !destinationMenuExpanded.value
                                 action(RideRequestAction.ChangeDestination(address = chosenDestination))
                             },
-                            leadingIcon = { Icon(Icons.Outlined.Edit, contentDescription = null) }
+                            leadingIcon = {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.ic_location_line),
+                                    contentDescription = null
+                                )
+                            }
                         )
                     }
                 }
@@ -206,7 +268,8 @@ private fun RequestRideScreenContent(
                 onClick = {
                     action(RideRequestAction.GetRideEstimate)
                 },
-                text = "Request Ride"
+                text = "Request Ride",
+                isLoading = state.isLoading
             )
         }
     }
@@ -218,6 +281,9 @@ private fun RequestRideScreenContent(
 private fun RequestRideScreenPreview() {
     RequestRideScreenContent(
         state = RideRequestState(),
-        action = {}
+        action = {},
+        context = LocalContext.current,
+        scope = rememberCoroutineScope(),
+        snackbarHostState = remember { SnackbarHostState() }
     )
 }
