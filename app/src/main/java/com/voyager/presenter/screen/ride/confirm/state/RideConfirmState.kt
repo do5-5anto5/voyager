79fb67ae7bed49b5e1af707cc3bedc5c338b7b42
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-12-12 20:11:57 -0300

    refactor: initial implementation on logic to onLoad and confirmRide in `RideRequestScreen`

--- app/src/main/java/com/voyager/presenter/screen/ride/confirm/state/RideConfirmState.kt
+++ app/src/main/java/com/voyager/presenter/screen/ride/confirm/state/RideConfirmState.kt
@@ -1,9 +1,22 @@
 package com.voyager.presenter.screen.ride.confirm.state
 
+import com.voyager.core.enums.FeedBackType
+import com.voyager.data.remote.model.ride.request.confirm.RideConfirmRequestBody
+import com.voyager.domain.model.ride.error.RideEstimateError
 import com.voyager.domain.model.ride.estimate.Option
+import com.voyager.domain.model.ride.estimate.RideEstimate
 
 
 data class RideConfirmState(
     val isLoading: Boolean = false,
-    val options: List<Option> = emptyList()
+    val options: List<Option> = emptyList(),
+    val requestBody: RideConfirmRequestBody = RideConfirmRequestBody(),
+    val errorResponseBody: String = "",
+    val error : RideEstimateError? = null,
+    val rideEstimate: RideEstimate? = null,
+    val id: String = "",
+    val origin: String = "",
+    val destination: String = "",
+    val hasFeedBack: Boolean = false,
+    val feedBack: Pair<FeedBackType, String>? = null
 )
